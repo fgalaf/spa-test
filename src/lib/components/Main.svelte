3c8692a1d5be5d8fe18b4f76eb41d420e43faa3a
Filip Gala <filip.gala@student.tuke.sk>
2025-12-07 10:32:37 +0100

    refactor Main.svelte

--- src/lib/components/Main.svelte
+++ src/lib/components/Main.svelte
@@ -1,43 +1,24 @@
 <script>
   import { t } from "$lib/i18n/i18n";
   export let reverse = false;
+
+  $: headerKey = reverse ? $t("header_1") : $t("header_0");
+  $: textKey = reverse ? $t("main_text_1") : $t("main_text_0");
+  $: colClass = reverse ? "sm:col-start-1 sm:ml-16" : "sm:col-start-3 sm:mr-16";
 </script>
 
 <main>
   <div class="grid sm:grid-cols-3 items-center min-h-[300px]">
 
-    {#if reverse}
-      <!-- TEXT ON LEFT -->
-      <div class="sm:ml-16 col-span-1 backdrop-blur-xs p-4 rounded-xl transition-transform duration-300 hover:scale-[1.03]">
-        <h1 class="text-3xl sm:text-4xl font-bold">
-          {$t("header_1")}
-        </h1>
+    <div class={`col-span-1 ${colClass} backdrop-blur-xs p-4 rounded-xl transition-transform duration-300 hover:scale-[1.03]`}>
+      <h1 class="text-3xl sm:text-4xl font-bold">
+        {headerKey}
+      </h1>
 
-        <p class="text-xl mt-2">
-          {@html $t("main_text_1")}
-        </p>
-      </div>
-
-      <!-- EMPTY MIDDLE + RIGHT -->
-      <div></div>
-      <div></div>
-
-    {:else}
-      <!-- EMPTY LEFT + MIDDLE -->
-      <div></div>
-      <div></div>
-
-      <!-- TEXT ON RIGHT -->
-      <div class="sm:mr-16 col-span-1 backdrop-blur-xs p-4 rounded-xl transition-transform duration-300 hover:scale-[1.03]">
-        <h1 class="text-3xl sm:text-4xl font-bold">
-          {$t("header_0")}
-        </h1>
-
-        <p class="text-xl mt-2">
-          {@html $t("main_text_0")}
-        </p>
-      </div>
-    {/if}
+      <p class="text-xl mt-2">
+        {textKey}
+      </p>
+    </div>
 
   </div>
 </main>
